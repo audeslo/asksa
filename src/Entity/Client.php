50480a2a8895197bfc7d93df2ba99aeed64fc803
Boris BODJRENOU <dehouenouboris@gmail.com>
2019-11-07 20:09:19 +0100

    Update 1.8

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -3,9 +3,14 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ClientRepository")
+ * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity(fields={"nomcomplet"}, message="Ce client existe déjà")
  */
 class Client
 {
@@ -18,11 +23,13 @@ class Client
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez saisir le referent !")
      */
     private $nom;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez saisir le nom complet !")
      */
     private $prenom;
 
@@ -45,6 +52,81 @@ class Client
      * @ORM\Column(type="string", length=255)
      */
     private $telephone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     */
+    private $adresserue;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *@Assert\NotBlank(message="Veuillez renseigner le type du client !")
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $telephone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $codepostal;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     * @Gedmo\Slug(fields={"nomcomplet"})
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdOn;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     */
+    private $createdBy;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $editedOn;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     */
+    private $editedBy;
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function datecreated()
+    {
+        $this->setCreatedOn(new \DateTime('now'));
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function dateupdated()
+    {
+        $this->setEditedOn(new \DateTime('now'));
+    }
+
 
     public function getId(): ?int
     {
@@ -119,6 +201,138 @@ class Client
     public function setTelephone(string $telephone): self
     {
         $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getAdresserue(): ?string
+    {
+        return $this->adresserue;
+    }
+
+    public function setAdresserue(?string $adresserue): self
+    {
+        $this->adresserue = $adresserue;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(?string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCodepostal(): ?string
+    {
+        return $this->codepostal;
+    }
+
+    public function setCodepostal(?string $codepostal): self
+    {
+        $this->codepostal = $codepostal;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(?string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getCreatedOn(): ?\DateTimeInterface
+    {
+        return $this->createdOn;
+    }
+
+    public function setCreatedOn(\DateTimeInterface $createdOn): self
+    {
+        $this->createdOn = $createdOn;
+
+        return $this;
+    }
+
+    public function getEditedOn(): ?\DateTimeInterface
+    {
+        return $this->editedOn;
+    }
+
+    public function setEditedOn(?\DateTimeInterface $editedOn): self
+    {
+        $this->editedOn = $editedOn;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?User
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?User $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getEditedBy(): ?User
+    {
+        return $this->editedBy;
+    }
+
+    public function setEditedBy(?User $editedBy): self
+    {
+        $this->editedBy = $editedBy;
 
         return $this;
     }
--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,50 @@ class ClientType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('ville')
-            ->add('adresse')
-            ->add('codePostal')
-            ->add('telephone')
+            ->add('referent', TextType::class, array(
+                'label'     => 'Réferent :',
+                'required'  => false,
+                'attr'      =>['placeholder'    =>  'Saisissez le referent']
+            ))
+            ->add('nomcomplet', TextType::class, array(
+                'label'     => 'Nom complet :',
+                'required'  => false,
+                'attr'      =>['placeholder'    =>  'Saisissez le nom complet']
+            ))
+            ->add('adresserue', TextType::class, array(
+                'label'     => 'Adresse rue :',
+                'required'  => false,
+                'attr'      =>['placeholder'    =>  'Saisissez le sigle']
+            ))
+            ->add('type', ChoiceType::class, array(
+                'choices'     => array(['Personne Morale' => 'Personne Morale'],
+                    ['Personne Physique' => 'Personne Physique']
+                ),
+                'label'     => 'Type du client :',
+                'required'  => false,
+                'attr'      =>['placeholder'    =>  'Saisissez le type client']
+            ))
+            ->add('telephone', TextType::class, array(
+                'label'     => 'Téléphone :',
+                'required'  => false,
+                'attr'      =>['placeholder'    =>  'Saisissez le sigle']
+            ))
+            ->add('ville', TextType::class, array(
+                'label'     => 'Ville :',
+                'required'  => false,
+                'attr'      =>['placeholder'    =>  'Saisissez le sigle']
+            ))
+            ->add('codepostal', TextType::class, array(
+                'label'     => 'Code postal :',
+                'required'  => false,
+                'attr'      =>['placeholder'    =>  'Saisissez le sigle']
+            ))
+            ->add('mail', TextType::class, array(
+                'label'     => 'E-mail :',
+                'required'  => false,
+                'attr'      =>['placeholder'    =>  'Saisissez le sigle']
+            ))
+
         ;
     }
 
