4b28c5a2629c5f3e51ebb7714852913ae7d3ba9d
Boris BODJRENOU <dehouenouboris@gmail.com>
2019-11-26 21:06:55 +0100

    Version3.3

---
+++ src/Migrations/Version20191125201015.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191125201015 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE commandershow CHANGE edited_on edited_on DATETIME NOT NULL, CHANGE created_on created_on DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE produit ADD img LONGBLOB DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE commandershow CHANGE edited_on edited_on DATETIME DEFAULT NULL, CHANGE created_on created_on DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE produit DROP img');
+    }
+}
